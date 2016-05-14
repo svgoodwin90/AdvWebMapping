a7828a55d2e0c823f5dc206064f12589b00dda98
svgoodwin90 <goodwin.scotty@gmail.com>
2016-05-14 17:21:08 -0400

    updated styles and content

--- PharmTradeFinal/script.js
+++ PharmTradeFinal/script.js
@@ -6,9 +6,9 @@ var sql = new cartodb.SQL({
 });
 var type = "'Total'";
 var partner = "'both'";
-var numCommas = function(){};
-var loadWindowInfo = function(){};
-var loadLineCharts = function(){};
+var numCommas = function() {};
+var loadWindowInfo = function() {};
+var loadLineCharts = function() {};
 
 $(document).ready(function() {
 	cartodb.createVis('map', 'https://sgoodwin.cartodb.com/api/v2/viz/f1da5f84-1796-11e6-94e1-0ecfd53eb7d3/viz.json', {
@@ -17,7 +17,8 @@ $(document).ready(function() {
 			cartodb_logo: false,
 			infowindow: true,
 			search: false,
-			shareable: false
+			shareable: false,
+			legends: false
 		})
 		.done(function(vis, layers) {
 
@@ -59,14 +60,14 @@ $(document).ready(function() {
 			///Hover infowindow reveal
 			vis.addOverlay({
 				type: 'tooltip',
-				position: 'top|right',
-				template: '<p class = "label">{{portcity}}, {{portstate}}</p>'
+				position: 'top|left',
+				template: '<p>{{portcity}}, {{portstate}}</p>'
 			});
 
 			///Click event on feature: 
 			tradeLayer.on('featureClick', function(e, latlng, pos, data, layerNumber) {
 				numCommas = function(x) {
-    				return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+					return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 				};
 				///Show and localize leaflet map
 				$('#map2').show();
@@ -76,107 +77,134 @@ $(document).ready(function() {
 				///Query and load data into template
 				loadWindowInfo = function() {
 					sql.execute("SELECT * FROM americanborder_trade WHERE portcode =" + data.portcode + "AND year =" + year + "ORDER BY percent DESC")
-					.done(function(data) {
-						console.log(data);
-						var template = $('#templateValues').html();
-						var context = {
-							portname: data.rows[0],
-							total: numCommas(data.rows[0].tradevalue),
-							ptotal: numCommas(data.rows[0].pharmvalue),
-							percenttotal: Math.floor(data.rows[0].percent * 1000) /1000,
-							imports: numCommas(data.rows[1].tradevalue),
-							pimports: numCommas(data.rows[1].pharmvalue),
-							percentim: Math.floor(data.rows[1].percent * 1000) /1000,
-							exports: numCommas(data.rows[2].tradevalue),
-							pexports: numCommas(data.rows[2].pharmvalue),
-							percentex: Math.floor(data.rows[2].percent * 1000) /1000,
-						};
-						var output = Mustache.render(template, context);
-						$('.results').show();
-						$('.results').append(output);
-					});
+						.done(function(data) {
+							console.log(data);
+							var template = $('#templateValues').html();
+							var context = {
+								year: year,
+								portname: data.rows[0],
+								total: numCommas(data.rows[0].tradevalue),
+								ptotal: numCommas(data.rows[0].pharmvalue),
+								percenttotal: Math.floor(data.rows[0].percent * 1000) / 1000,
+								imports: numCommas(data.rows[1].tradevalue),
+								pimports: numCommas(data.rows[1].pharmvalue),
+								percentim: Math.floor(data.rows[1].percent * 1000) / 1000,
+								exports: numCommas(data.rows[2].tradevalue),
+								pexports: numCommas(data.rows[2].pharmvalue),
+								percentex: Math.floor(data.rows[2].percent * 1000) / 1000,
+							};
+							var output = Mustache.render(template, context);
+							$('.results').show();
+							$('.results').append(output);
+						});
 				};
 				loadLineCharts = function() {
+					$('#stats-label').empty();
 					sql.execute("SELECT * FROM americanborder_trade WHERE portcode =" + data.portcode + "AND type =" + type + "ORDER BY percent DESC")
-					.done(function(data){
-						var years = ['Year'];
-						var tvalue = ['Total Trade Value']; 
-						var pvalue = ['Pharmaceutical Trade Value'];
-						for (var i = 0; i < 9; i++){
-							years[i+1] = data.rows[i].year;
-							tvalue[i+1] = data.rows[i].tradevalue;
-							pvalue[i+1] = data.rows[i].pharmvalue;
-						}
-						var columns1 = [years, tvalue];
-						var columns2 = [years, pvalue];
-						var chart1 = c3.generate({
-							bindto: ('#chart1'),
-							data: {
-								x: 'Year',
-								columns: columns1
-							},
-							axis: {
-								y: {
-									tick: {
-										format: d3.format("$,")
-									}
-								}
-							},
-							size: {
-								width: 340,
-								height: 200
-							},
-							color: {
-    							pattern: ['#cc0000']
+						.done(function(data) {
+							var years = ['Year'];
+							var tvalue = ['Total Trade Value'];
+							var pvalue = ['Pharmaceutical Trade Value'];
+							for (var i = 0; i < 9; i++) {
+								years[i + 1] = data.rows[i].year;
+								tvalue[i + 1] = data.rows[i].tradevalue;
+								pvalue[i + 1] = data.rows[i].pharmvalue;
 							}
-						});
-						var chart2 = c3.generate({
-							bindto: ('#chart2'),
-							data: {
-								x: 'Year',
-								columns: columns2
-							},
-							axis: {
-								y: {
-									tick: {
-										format: d3.format("$,")
+							var columns1 = [years, tvalue];
+							var columns2 = [years, pvalue];
+							var chart1 = c3.generate({
+								bindto: ('#chart1'),
+								data: {
+									x: 'Year',
+									columns: columns1
+								},
+								axis: {
+									y: {
+										tick: {
+											format: d3.format("$,")
+										}
 									}
+								},
+								size: {
+									width: 340,
+									height: 200
+								},
+								color: {
+									pattern: ['#cc0000']
 								}
-							},
-							size: {
-								width: 340,
-								height: 200
-							},
-							color: {
-    							pattern: ['#cc0000']
-							}
+							});
+							var chart2 = c3.generate({
+								bindto: ('#chart2'),
+								data: {
+									x: 'Year',
+									columns: columns2
+								},
+								axis: {
+									y: {
+										tick: {
+											format: d3.format("$,")
+										}
+									}
+								},
+								size: {
+									width: 340,
+									height: 200
+								},
+								color: {
+									pattern: ['#cc0000']
+								}
+							});
+							var template2 = $('#templateValues2').html();
+							var context2 = {
+								portname: data.rows[0]
+							};
+							var output2 = Mustache.render(template2, context2);
+							$('#stats-label').append(output2);
 						});
-					});
 				};
-				$('#stats-window').hide();
-				$('#summary-window').show();
+				$('.overview').hide();
+				$('.statistics').hide();
+				$('.summary').show();
 				loadLineCharts();
 				loadWindowInfo();
 			});
 		});
 
-	$('#summary-window').hide();
-	$('#stats-window').hide();
+	$('.summary').hide();
+	$('.statistics').hide();
+	$('.sources').hide();
 
-	///Arrows on click, reveal stats or summary windows
-	$('#statsbutton').on('click', function() {
+	///Arrows on click, reveal stats, summary or sources windows
+	$('#statsbutton1').on('click', function() {
 		$('#map2').hide();
-		$('#summary-window').hide();
-		$('#stats-window').show();
+		$('.summary').hide();
+		$('.statistics').show();
 	});
 
 	$('#sumbutton').on('click', function() {
-		$('#stats-window').hide();
+		$('.statistics').hide();
 		$('#map2').show();
 		$('.results').show();
-		$('#summary-window').show();
+		$('.summary').show();
 		loadLineCharts();
 	});
+
+	$('#sourcebutton').on('click', function() {
+		$('#map2').hide();
+		$('.summary').hide();
+		$('.statistics').hide();
+		$('.sources').show();
+	});
+
+	$('#statsbutton2').on('click', function() {
+		$('#map2').hide();
+		$('.summary').hide();
+		$('.sources').hide();
+		$('.statistics').show();
+	});
+
+
+
 
 	///Initialize slider
 	$(function() {
@@ -186,17 +214,17 @@ $(document).ready(function() {
 			max: 2015,
 			step: 1,
 			slide: function(event, ui) {
-				$('#amount').val(ui.value);
+				$('#year-selector').val(ui.value);
 			}
 		});
-		$('#amount').val($('#slider').slider('value'));
+		$('#year-selector').val($('#slider').slider('value'));
 	});
 
 	///Change event on selectors 
 	///Query data on change	  
 	$('#partnerSelect').add('#tradeSelect').add('#slider').on('change slidechange', function(ui) {
 		///Slider change
-		year = $('#amount').val();
+		year = $('#year-selector').val();
 		$('.results').empty();
 		///Selector changes
 		type = tradeSelect.value;
